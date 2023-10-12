369a04adf22dfa9e15b486fafa089c24acc2b9dc
Liaksej <tarusov.liaksej@gmail.com>
2023-10-13 00:36:52 +0200

    Fix UI and functionalities using new modules
    The commit updated multiple files mainly to improve the UI and enhance functionalities.
    
    Main changes include, in layout.tsx, the Inter font was replaced with a Roboto font of multiple weighted variants. Other changes include new @emotion modules added into the dependencies for robust styled-components and Material UI icons for enhanced UI.
    
    Additionally, the 'build' script was modified for Jekyll compatibility. This allows easy static hosting, especially suitable on platforms like GitHub.
    
    The project's base path has also been modified to align with production needs. These changes particularly improve the deployment process of the app. The README was significantly simplifies and the project related details were updated.
    
    New dependencies, related to the aforementioned changes were subsequently added in respective files, package.json and pnpm-lock.yaml.
    
    Overall, these changes collectively aim to provide a more efficient, scalable, and user-friendly application.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/ra-lifecycle-http-crud-client"
+      : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
